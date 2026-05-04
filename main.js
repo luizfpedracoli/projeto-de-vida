ab41b05fefc719f2b735121d813470a3ce36aa1d
luizfpedracoli <166130464+luizfpedracoli@users.noreply.github.com>
2026-05-04 12:34:29 -0300

    s

--- main.js
+++ main.js
@@ -15,10 +15,12 @@ textos[i].classList.add("ativo");
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-04-20T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2026-03-20T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2026-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2026-06-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
 let tempoAtual = new Date();
@@ -27,6 +29,11 @@ let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+if (tempoFinal > 0) {
 return (
 dias +
 " dias " +
@@ -37,4 +44,20 @@ minutos +
 segundos +
 " segundos"
 );
+} else {
+return "Prazo Finalizado";
 }
+}
+
+function atualizaCronometro() {
+for (let i = 0; i < contadores.length; i++) {
+contadores[i].textContent = calculaTempo(tempos[i]);
+}
+}
+
+function comecaCronometro() {
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
+}
+
+//comecaCronometro();
